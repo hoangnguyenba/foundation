de8552e7ad02ee86d9774700800a74050cd367a8
Hoang <hoangnguyen@bravesoft.vn>
2016-11-09 18:42:05 +0700

    Add vagrant config

--- Vagrantfile
+++ Vagrantfile
@@ -1,71 +1,57 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# All Vagrant configuration is done below. The "2" in Vagrant.configure
-# configures the configuration version (we support older styles for
-# backwards compatibility). Please don't change it unless you know what
-# you're doing.
-Vagrant.configure("2") do |config|
-  # The most common configuration options are documented and commented below.
-  # For a complete reference, please see the online documentation at
-  # https://docs.vagrantup.com.
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
 
-  # Every Vagrant development environment requires a box. You can search for
-  # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "hashicorp/precise64"
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-  # Disable automatic box update checking. If you disable this, then
-  # boxes will only be checked for updates when the user runs
-  # `vagrant box outdated`. This is not recommended.
-  # config.vm.box_check_update = false
+  # Base Box
+  # --------------------
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.hostname = "phalcon.local"
 
-  # Create a forwarded port mapping which allows access to a specific port
-  # within the machine from a port on the host machine. In the example below,
-  # accessing "localhost:8080" will access port 80 on the guest machine.
-  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  # Connect to IP
+  # Note: Use an IP that doesn't conflict with any OS's DHCP (Below is a safe bet)
+  # --------------------
+  config.vm.network :private_network, ip: "192.168.50.4"
 
-  # Create a private network, which allows host-only access to the machine
-  # using a specific IP.
-  # config.vm.network "private_network", ip: "192.168.33.10"
+  # Forward to Port
+  # --------------------
+  config.vm.network :forwarded_port, guest: 3306, host: 3306, auto_correct: true
 
-  # Create a public network, which generally matched to bridged network.
-  # Bridged networks make the machine appear as another physical device on
-  # your network.
-  # config.vm.network "public_network"
+  # Optional (Remove if desired)
+  # --------------------
+  config.vm.provider :virtualbox do |vb|
+    vb.customize [
+      "modifyvm", :id,
+      "--memory", 512,             # How much RAM to give the VM (in MB)
+      "--cpus", 1,                 # Muli-core in the VM
+      "--ioapic", "on",
+      "--natdnshostresolver1", "on",
+      "--natdnsproxy1", "on"
+    ]
+  end
 
-  # Share an additional folder to the guest VM. The first argument is
-  # the path on the host to the actual folder. The second argument is
-  # the path on the guest to mount the folder. And the optional third
-  # argument is a set of non-required options.
-  # config.vm.synced_folder "../data", "/vagrant_data"
+  # If true, agent forwarding over SSH connections is enabled
+  # --------------------
+  config.ssh.forward_agent = true
 
-  # Provider-specific configuration so you can fine-tune various
-  # backing providers for Vagrant. These expose provider-specific options.
-  # Example for VirtualBox:
-  #
-  # config.vm.provider "virtualbox" do |vb|
-  #   # Display the VirtualBox GUI when booting the machine
-  #   vb.gui = true
-  #
-  #   # Customize the amount of memory on the VM:
-  #   vb.memory = "1024"
-  # end
-  #
-  # View the documentation for the provider you are using for more
-  # information on available options.
+  # The shell to use when executing SSH commands from Vagrant
+  # --------------------
+  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
 
-  # Define a Vagrant Push strategy for pushing to Atlas. Other push strategies
-  # such as FTP and Heroku are also available. See the documentation at
-  # https://docs.vagrantup.com/v2/push/atlas.html for more information.
-  # config.push.define "atlas" do |push|
-  #   push.app = "YOUR_ATLAS_USERNAME/YOUR_APPLICATION_NAME"
-  # end
+  # Synced Folders
+  # --------------------
+  #config.vm.synced_folder ".", "/vagrant/", :mount_options => [ "dmode=777", "fmode=666" ]
+  #config.vm.synced_folder "./www", "/vagrant/www/", :mount_options => [ "dmode=775", "fmode=644" ]
 
-  # Enable provisioning with a shell script. Additional provisioners such as
-  # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
-  # documentation for more information about their specific syntax and use.
-  # config.vm.provision "shell", inline: <<-SHELL
-  #   apt-get update
-  #   apt-get install -y apache2
-  # SHELL
+  # Provisioning Scripts
+  # --------------------
+  config.vm.provision "shell", path: "init.sh"
+  config.vm.provision :shell, inline: "sleep 10; service postgresql restart;"
+  config.vm.provision :shell, inline: "service redis-server restart;"
+  config.vm.provision :shell, inline: "service mysql restart;"
+  config.vm.provision :shell, inline: "service apache2 restart;", run: "always"
+  config.vm.provision :shell, inline: "service mongodb restart;"
 end
---
+++ init.sh
@@ -0,0 +1,258 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+export COMPOSER_ALLOW_SUPERUSER=1
+export ZEPHIRDIR=/usr/share/zephir
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+#
+# Add Swap
+#
+dd if=/dev/zero of=/swapspace bs=1M count=4000
+mkswap /swapspace
+swapon /swapspace
+echo "/swapspace none swap defaults 0 0" >> /etc/fstab
+
+echo nameserver 8.8.8.8 > /etc/resolv.conf
+echo nameserver 8.8.4.4 > /etc/resolv.conf
+
+#
+# Add PHP and PostgreSQL repositories
+#
+LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php
+apt-add-repository -y ppa:chris-lea/libsodium
+add-apt-repository -y ppa:chris-lea/redis-server
+touch /etc/apt/sources.list.d/pgdg.list
+echo -e "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" | tee -a /etc/apt/sources.list.d/pgdg.list &>/dev/null
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+# Cleanup package manager
+apt-get clean -y
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+apt-get update -qq
+apt-get upgrade -y
+apt-get install -y build-essential software-properties-common python-software-properties
+
+#
+# Setup locales
+#
+echo -e "LC_CTYPE=en_US.UTF-8\nLC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8" | tee -a /etc/environment &>/dev/null
+locale-gen en_US en_US.UTF-8
+dpkg-reconfigure locales
+
+#
+# Base system
+#
+apt-get install -yq --no-install-suggests --no-install-recommends \
+  mysql-server-5.6 \
+  mysql-client-5.6 \
+  apache2 \
+  libapache2-mod-php5.6 \
+  memcached \
+  mongodb-clients \
+  mongodb-server \
+  postgresql-9.4 \
+  sqlite3 \
+  beanstalkd \
+  libyaml-dev \
+  libsodium-dev \
+  curl \
+  htop \
+  git \
+  dos2unix \
+  unzip \
+  vim \
+  mc \
+  grc \
+  gcc \
+  make \
+  re2c \
+  libpcre3 \
+  libpcre3-dev \
+  lsb-core \
+  autoconf \
+  redis-server \
+  redis-tools
+
+#
+# Base PHP
+#
+apt-get install -yq --no-install-suggests --no-install-recommends \
+  php5.6 \
+  php5.6-apcu \
+  php5.6-bcmath \
+  php5.6-bz2 \
+  php5.6-cli \
+  php5.6-common \
+  php5.6-curl \
+  php5.6-dba \
+  php5.6-dev \
+  php5.6-gd \
+  php5.6-gearman \
+  php5.6-gettext \
+  php5.6-gmp \
+  php5.6-imagick \
+  php5.6-imap \
+  php5.6-intl \
+  php5.6-json \
+  php5.6-mbstring \
+  php5.6-memcached \
+  php5.6-memcache \
+  php5.6-mcrypt \
+  php5.6-mongo \
+  php5.6-mongodb \
+  php5.6-mysql \
+  php-pear \
+  php5.6-odbc \
+  php5.6-pgsql \
+  php5.6-ps \
+  php5.6-pspell \
+  php5.6-redis \
+  php5.6-readline \
+  php5.6-recode \
+  php5.6-soap \
+  php5.6-sqlite3 \
+  php5.6-tidy \
+  php5.6-xdebug \
+  php5.6-xmlrpc \
+  php5.6-xsl \
+  php5.6-zip
+
+echo "apc.enable_cli = 1" >> /etc/php/5.6/mods-available/apcu.ini
+
+#
+# Update PECL channel
+#
+pecl channel-update pecl.php.net
+
+#
+# Tune Up Postgres
+#
+cp /etc/postgresql/9.4/main/pg_hba.conf /etc/postgresql/9.4/main/pg_hba.bkup.conf
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres'" &>/dev/null
+sed -i.bak -E 's/local\s+all\s+postgres\s+peer/local\t\tall\t\tpostgres\t\ttrust/g' /etc/postgresql/9.4/main/pg_hba.conf
+
+#
+# YAML
+#
+(CFLAGS="-O1 -g3 -fno-strict-aliasing"; pecl install yaml < /dev/null &)
+touch /etc/php/5.6/mods-available/yaml.ini
+echo 'extension=yaml.so' | tee /etc/php/5.6/mods-available/yaml.ini &>/dev/null
+
+#
+# Libsodium
+#
+pecl install -a libsodium < /dev/null &
+touch /etc/php/5.6/mods-available/libsodium.ini
+echo 'extension=libsodium.so' | tee /etc/php/5.6/mods-available/libsodium.ini &>/dev/null
+
+#
+# Zephir
+#
+echo "export ZEPHIRDIR=/usr/share/zephir" >> /home/vagrant/.bashrc
+mkdir -p ${ZEPHIRDIR}
+phpdismod xdebug redis
+(cd /tmp && git clone git://github.com/phalcon/zephir.git && cd zephir && ./install -c)
+chown -R vagrant:vagrant ${ZEPHIRDIR}
+
+#
+# Install Phalcon Framework
+#
+git clone --depth=1 git://github.com/phalcon/cphalcon.git
+(cd cphalcon && zephir build)
+touch /etc/php/5.6/mods-available/phalcon.ini
+echo -e "extension=phalcon.so" | tee /etc/php/5.6/mods-available/phalcon.ini &>/dev/null
+
+#
+# Tune Up Redis
+#
+cp /etc/redis/redis.conf /etc/redis/redis.bkup.conf
+sed -i 's/bind 127.0.0.1/bind 0.0.0.0/' /etc/redis/redis.conf
+
+#
+# Tune Up MySQL
+#
+cp /etc/mysql/my.cnf /etc/mysql/my.bkup.cnf
+sed -i 's/bind-address/bind-address = 0.0.0.0#/' /etc/mysql/my.cnf
+mysql -u root -Bse "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION;"
+
+#
+# Composer for PHP
+#
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+#
+# Apache VHost
+#
+cd ~
+echo '<VirtualHost *:80>
+        DocumentRoot /vagrant
+        ErrorLog  /vagrant/projects-error.log
+        CustomLog /vagrant/projects-access.log combined
+</VirtualHost>
+
+<Directory "/vagrant">
+        Options Indexes Followsymlinks
+        AllowOverride All
+        Require all granted
+</Directory>' > vagrant.conf
+
+mv vagrant.conf /etc/apache2/sites-available
+a2enmod rewrite
+
+#
+# Install Phalcon DevTools
+#
+cd ~
+echo '{"require": {"phalcon/devtools": "dev-master"}}' > composer.json
+composer install --ignore-platform-reqs
+rm composer.json
+mkdir /opt/phalcon-tools
+mv ~/vendor/phalcon/devtools/* /opt/phalcon-tools
+rm -rf ~/vendor
+echo "export PTOOLSPATH=/opt/phalcon-tools/" >> /home/vagrant/.bashrc
+echo "export PATH=\$PATH:/opt/phalcon-tools/" >> /home/vagrant/.bashrc
+chmod +x /opt/phalcon-tools/phalcon.sh
+ln -s /opt/phalcon-tools/phalcon.sh /usr/bin/phalcon
+
+#
+# Tune UP PHP
+#
+sed -i 's/short_open_tag = Off/short_open_tag = On/' /etc/php/5.6/apache2/php.ini
+sed -i 's/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_ALL/' /etc/php/5.6/apache2/php.ini
+sed -i 's/display_errors = Off/display_errors = On/' /etc/php/5.6/apache2/php.ini
+sed -i '/\[Session\]/a session.save_path = "/tmp"' /etc/php/5.6/apache2/php.ini
+phpenmod -v 5.6 -s ALL yaml mcrypt intl curl libsodium phalcon soap redis xdebug
+
+#
+# Tune Up Apache
+#
+sed -i 's/export APACHE_RUN_USER=www-data/export APACHE_RUN_USER=vagrant/' /etc/apache2/envvars
+sed -i 's/export APACHE_RUN_GROUP=www-data/export APACHE_RUN_GROUP=vagrant/' /etc/apache2/envvars
+
+#
+# Reload apache
+#
+a2ensite vagrant
+a2dissite 000-default
+
+#
+#  Cleanup
+#
+apt-get autoremove -y
+apt-get autoclean -y
+
+echo -e "----------------------------------------"
+echo -e "To create a Phalcon Project:\n"
+echo -e "----------------------------------------"
+echo -e "$ cd /vagrant/www"
+echo -e "$ phalcon project <projectname>\n"
+echo -e
+echo -e "Then follow the README.md to copy/paste the VirtualHost!\n"
+
+echo -e "----------------------------------------"
+echo -e "Default Site: http://192.168.50.4"
+echo -e "----------------------------------------"
